0763dc695ad4a4c1a92fa2d73a265a0816b27be6
Akita <noahrusso83@gmail.com>
2025-02-10 12:34:11 +0100

    Export ONC global var only after the package is loaded

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -1,4 +1,8 @@
 ONC = ONC or {}
 
-Package.Require("./config/modules.lua")
-Package.Require("./module_loader.lua")
+Package.Require("/config/modules.lua")
+Package.Require("/module_loader.lua")
+
+Package.Subscribe("Load", function()
+    Package.Export("ONC", ONC)
+end)
