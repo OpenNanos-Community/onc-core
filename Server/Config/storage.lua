00392676f7f1291e7e534f01e8d0aa02d593e5f1
Akita <noahrusso83@gmail.com>
2025-02-10 12:20:55 +0100

    Change config file for storage

--- Server/Config/storage.lua
+++ Server/Config/storage.lua
@@ -1,5 +1,10 @@
--- Due to the sensibility of the data, the variable is local.
-local tSqlCredentials = {
+local tSql = {}
+
+-- Activate or deactivate the SQL registration (true/false)
+-- Information: The SQL registration is the best way to store data, it is fully recommended to use it. (increase the performance of your server)
+tSql.Activate = false 
+
+tSql.Credentials = {
     databaseType = "SQLite", -- SQLite / MySQL / PostgreSQL (write the name of the database type you are using exacly as it is written here)
     host = "",
     user = "",
@@ -8,3 +13,4 @@ local tSqlCredentials = {
     port = 3306
 }
 
+return tSql
