8759cc87e78cea10607fcdd02cc28eb1b96ad541
Akita <noahrusso83@gmail.com>
2025-02-10 11:11:15 +0100

    Some typo change

--- Shared/module_loader.lua
+++ Shared/module_loader.lua
@@ -1,56 +1,60 @@
-local shouldStop = false
+local bShouldStop = false
 
-local function checkStopCode(returnedCode)
-   if (returnedCode == -1) then
-    shouldStop = true
+-- Function used internally
+-- Check if the return code is -1, if so, stop the package
+local function CheckStopCode(iReturnedCode)
+   if (iReturnedCode == -1) then
+    bShouldStop = true
     Server.UnloadPackage(Package.GetName())
    end
 end
 
-local function loadModules()
-    -- mName : module name && mConfig : module configuration(ex: files, displayName)
-    for k, mConfig in ipairs(ONC.Modules) do
-        Console.Log("[Loader] Loading module: " ..mConfig.pathName)
+-- Function used internally
+-- Load all modules
+local function LoadModules()
+    -- mName : module name && tConfig : module configuration(ex: files, displayName)
+    for k, tConfig in ipairs(ONC.Modules) do
+        Console.Log("[Loader] Loading module: " .. tConfig.pathName)
 
-        local isServer = _ENV["Server"] ~= nil and true or false;
-        local moduleFiles = mConfig.files
-        local packageName = Package.GetName()
+        local bIsServer = _ENV["Server"] ~= nil and true or false
+        local tModuleFiles = tConfig.files
+        local sPackageName = Package.GetName()
 
         -- Shared include
-        for _, sharedFile in ipairs(moduleFiles.shared) do
-            if (shouldStop) then return end
+        for _, sSharedFile in ipairs(tModuleFiles.shared) do
+            if (bShouldStop) then return end
 
-            Console.Log("\tLoading SH file: " ..sharedFile)
-            returnCode = Package.Require(packageName.. "/Shared/modules/" ..mConfig.pathName.. "/" ..sharedFile)
+            Console.Log("\tLoading SH file: " ..sSharedFile)
+            returnCode = Package.Require(sPackageName.. "/Shared/modules/" ..tConfig.pathName.. "/" ..sSharedFile)
 
-            checkStopCode(returnCode)
+            CheckStopCode(returnCode)
 
         end
 
         -- Server include
-        if (isServer) then
-            for _, serverFile in ipairs(moduleFiles.server) do
-                if (shouldStop) then return end
+        if (bIsServer) then
+            for _, serverFile in ipairs(tModuleFiles.server) do
+                if (bShouldStop) then return end
 
                 Console.Log("\tLoading SV file: " ..serverFile)
-                returnCode = Package.Require(packageName.. "/Server/modules/" ..mConfig.pathName.. "/" ..serverFile)
+                returnCode = Package.Require(sPackageName.. "/Server/modules/" ..tConfig.pathName.. "/" ..serverFile)
 
-                checkStopCode(returnCode)
+                CheckStopCode(returnCode)
             end
         end
 
         -- Client include
-        if (not isServer) then
-            for _, clientFile in ipairs(moduleFiles.client) do
-                if (shouldStop) then return end
+        if (not bIsServer) then
+            for _, clientFile in ipairs(tModuleFiles.client) do
+                if (bShouldStop) then return end
 
                 Console.Log("\tLoading CL file: " ..clientFile)
-                returnCode = Package.Require(packageName.. "/Client/modules/" ..mConfig.pathName.. "/" ..clientFile)
+                returnCode = Package.Require(sPackageName.. "/Client/modules/" ..tConfig.pathName.. "/" ..clientFile)
 
-                checkStopCode(returnCode)
+                CheckStopCode(returnCode)
             end
         end
     end
 end
 
-loadModules()
+LoadModules()
