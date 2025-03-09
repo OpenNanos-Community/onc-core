5b3bed99ba084b07e53003e9429a511fb1c235df
Certurix <46199479+Certurix@users.noreply.github.com>
2025-03-09 20:34:50 +0100

    fix: Set default input when package load

--- Client/Index.lua
+++ Client/Index.lua
@@ -1,2 +1,7 @@
 -- ONC Core HUD
 ONC.BaseHUD = WebUI("ONC_Core HUD", "file:///UI/index.html")
+
+Package.Subscribe("Load", function()
+	Input.SetMouseEnabled(false)
+	Input.SetInputEnabled(true)
+end)
