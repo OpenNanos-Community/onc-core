491a5013b2aaae79ed9a1c4c6b93e2ab5f49f308
Certurix <46199479+Certurix@users.noreply.github.com>
2025-03-16 20:05:41 +0100

    fix(ui): Updated the data values returned to the UpdateName Event

--- Client/Modules/core/cl_base.lua
+++ Client/Modules/core/cl_base.lua
@@ -1,3 +1,3 @@
 Events.SubscribeRemote("ONC::CharacterInformationUpdate", function(tData)
-    ONC.BaseHUD:CallEvent("ONC::UpdateName", Client.GetLocalPlayer():GetName())
+    ONC.BaseHUD:CallEvent("ONC::UpdateName", tData["firstname"].." "..tData["lastname"])
 end)
